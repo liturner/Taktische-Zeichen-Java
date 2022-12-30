180cda2613083d874290d378b1d1389e4b28b590
liturner <turnertech@outlook.de>
2022-12-30 10:58:15 +0100

    Site checkin for project Taktische-Zeichen-Java

--- taktische-zeichen/apidocs/type-search-index.js
+++ taktische-zeichen/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"de.turnertech.tz.symbol","l":"TacticalSymbol"},{"p":"de.turnertech.tz.symbol","l":"TacticalSymbolFactory"},{"p":"de.turnertech.tz.api","l":"TacticalSymbolTag"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"de.turnertech.tz.symbol","l":"TacticalSymbolResource"},{"p":"de.turnertech.tz.symbol","l":"TacticalSymbolResourceFactory"},{"p":"de.turnertech.tz.symbol","l":"TacticalSymbolTag"}];updateSearchResults();
